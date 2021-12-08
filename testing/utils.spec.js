743b8c32cecfd280cd92b9e918bc6090676b92b2
koebe1 <koe.be.pay@web.de>
2021-12-08 21:03:45 +0100

    testing

---
+++ testing/utils.spec.js
@@ -0,0 +1,37 @@
+const { expect } = require("@jest/globals");
+const { getNewUser, mapObjectToArray } = require("./utils");
+
+describe("mapObjectToArray(),", () => {
+  test("maps values to array using callback", () => {
+    const result = mapObjectToArray({ age: 30, height: 65 }, (k, v) => {
+      return v + 10;
+    });
+    // deep check array with toEqual
+    expect(result).toEqual([40, 75]);
+  });
+
+  test("callback gets called", () => {
+    const mockCb = jest.fn();
+    const result = mapObjectToArray({ age: 30, height: 65 }, mockCb);
+
+    expect(mockCb.mock.calls.length).toBe(2);
+  });
+});
+
+describe("getNewUser()", () => {
+  test("it gets user", async () => {
+    const user = await getNewUser(1);
+    expect(user).toBeTruthy();
+    expect(user.id).toBe(1);
+  });
+
+  test("no user found", async () => {
+    expect.assertions(1);
+
+    try {
+      const user = await getNewUser(19);
+    } catch (e) {
+      expect(e).toBeTruthy();
+    }
+  });
+});
